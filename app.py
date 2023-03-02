75f87c1fd977fbb0022a365e311541893ecd0097
gavsimps <gavsimps@iu.edu>
2023-03-02 14:42:46 -0500

    members done except for sorting

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import csv
+
 app = Flask(__name__)
+
+with open('members.csv','r') as csvfile:
+    lines = csv.reader(csvfile)
+    member_header = []
+    for row in lines:
+        member_header.append(row)
+
+with open('members.csv','r') as csvfile:
+    data = csv.DictReader(csvfile)
+    members_info = {row['Name']:{'Name':row['Name'],'DoB':row['DoB'],'Email':row['Email'],
+    'Address':row['Address'],'Phone':row['Phone']} for row in data}
+    
+with open('trips.csv','r') as csvfile:
+    read = csv.reader(csvfile)
+    trip_head = []
+    for row in read:
+        trip_head.append(row)
+    trips_info= {}
+
+
+
 @app.route('/')
-def hello_world():
-    return 'I can see you.'
+def index():
+    return render_template('index.html')
+
+@app.route('/members')
+def members():
+    return render_template('members.html',members_info=members_info,member_header=member_header)
+
+@app.route('/trips')
+def trips():
+    return render_template('trips.html')
